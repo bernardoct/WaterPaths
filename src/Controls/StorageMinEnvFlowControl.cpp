22d78249bc84e285fe30a1526da1b8d09d7c7b1a
bernardoct <12838200c5>
2017-06-29 09:34:24 -0400

    Implementing reservoir controls

--- src/Controls/StorageMinEnvFlowControl.cpp
+++ src/Controls/StorageMinEnvFlowControl.cpp
@@ -4,3 +4,12 @@
 
 #include "StorageMinEnvFlowControl.h"
 
+
+double StorageMinEnvFlowControl::getRelease(int week) {
+    double releases = 0;
+    for (int i = 0; i < week_thresholds->size(); ++i) {
+        /// Done with ternary operator for improved performance.
+        releases = (week <= (*week_thresholds)[i] ? min_env_flows[i] : releases);
+    }
+    return 0;
+}

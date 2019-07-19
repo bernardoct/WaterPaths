47d53598e5597b349c759fbc3bd1b50642308a79
Bernardo Trindade <bct52@cornell.edu>
2019-07-19 02:42:49 -0500

    non-working version of csv rof tables.

--- src/Problem/Base/Problem.cpp
+++ src/Problem/Base/Problem.cpp
@@ -6,8 +6,10 @@
 #include <numeric>
 #include <random>
 #include <set>
+#include <zconf.h>
 #include "Problem.h"
 #include "../../Utils/Utils.h"
+#include <omp.h>
 
 vector<double> Problem::calculateAndPrintObjectives(bool print_files) {
     if (this->master_data_collector != nullptr) {
@@ -121,7 +123,7 @@ void Problem::setRDMOptimization(vector<vector<double>> &utilities_rdm,
     this->policies_rdm = policies_rdm;
 }
 
-void Problem::setRDMReevaluation(unsigned long rdm_no, vector<vector<double>> &utilities_rdm,
+void Problem::setRDMReevaluation(int rdm_no, vector<vector<double>> &utilities_rdm,
                                  vector<vector<double>> &water_sources_rdm,
                                  vector<vector<double>> &policies_rdm) {
     this->rdm_no = rdm_no;
@@ -182,6 +184,98 @@ Problem::Problem(unsigned long n_weeks) : n_weeks(n_weeks) {
     Reservoir::unsetSeed();
 }
 
+///**
+// * Read pre-computed ROF tables.
+// * @param folder Folder containing the ROF tables.
+// * @param n_realizations number of realizations.
+// */
+//void
+//Problem::setRofTables(unsigned long n_realizations, string rof_tables_directory) {
+//
+//    set<unsigned long> s( realizations_to_run.begin(), realizations_to_run.end() );
+//    vector<unsigned long> realizations_to_run_load_tables;
+//    realizations_to_run_load_tables.assign( s.begin(), s.end() );
+//
+//    //double start_time = omp_get_wtime();
+//    cout << "Loading ROF tables" << endl;
+//    int n_tiers = NO_OF_INSURANCE_STORAGE_TIERS + 1;
+//    auto table_data_id = realizations_to_run_load_tables[0];
+//
+//    /// Get number of weeks in tables
+//    string file_name = rof_tables_directory + "/tables_r" + to_string(table_data_id) + "_u0";
+//    ifstream in(file_name, ios_base::binary);
+//    if (!in.good()) {
+//        string error_table_file = "Tables file not found: " + file_name;
+//        throw invalid_argument(error_table_file.c_str());
+//    }
+//
+//    //FIXME: FIGURE OUT NUMBER OF UTILITIES BY CHECKING IF TABLES_R0_U0, TABLES_R0_U1, AND SO ON EXIST.
+//    char table_file_name[150];
+//    for (n_utilities = 0; n_utilities < MAX_NUMBER_OF_UTILITIES; ++n_utilities) {
+//        sprintf(table_file_name, "%s%stables_r%lu_u%d",
+//                rof_tables_directory.c_str(), BAR.c_str(), table_data_id, n_utilities);
+//        ifstream f(table_file_name);
+//        if (!f.good()) {
+//            if (n_utilities == 0) {
+//                char error[200];
+//                sprintf(error, "Table %s not found.", table_file_name);
+//                throw invalid_argument(error);
+//            }
+//            break;
+//        }
+//    }
+//
+//    unsigned n_weeks_in_table;
+//    in.read(reinterpret_cast<char *>(&n_weeks_in_table), sizeof(unsigned));
+//
+//    // Create empty tables
+//    rof_tables = vector<vector<Matrix2D<double>>>(
+//            n_realizations,
+//            vector<Matrix2D<double>>((unsigned long) n_utilities,
+//                                     Matrix2D<double>(n_weeks_in_table / n_tiers, n_tiers)));
+//
+//    this->rof_tables_directory = rof_tables_directory;
+//
+//    // Load ROF tables
+//    for (auto r : realizations_to_run_load_tables) {
+//        for (int u = 0; u < n_utilities; ++u) {
+//            file_name = rof_tables_directory + BAR + "tables_r" + to_string(r) + "_u" + to_string(u);
+//            ifstream in_file(file_name, ios_base::binary);
+//            if (!in.good()) {
+//                string error_table_file = "Tables file not found: " + file_name;
+//                throw invalid_argument(error_table_file.c_str());
+//            }
+//
+//            // Get table file size from table files.
+//            unsigned stringsize;
+//            in_file.read(reinterpret_cast<char *>(&stringsize), sizeof(unsigned));
+//
+//            // Get table information from table files.
+//            double data[stringsize];
+//            in_file.read(reinterpret_cast<char *>(&data),
+//                    stringsize * sizeof(double));
+//
+//            // Create tables based on table files.
+//            rof_tables[r][u].setData(data, (int) stringsize);
+//
+//            for (unsigned long i = 0; i < stringsize; ++i) {
+//                double d = data[i];
+//                if (std::isnan(d) || d > 1.01 || d < 0) {
+//                    string error_m = "nan or out of [0,1] rof imported "
+//                                     "tables. Realization " +
+//                                     to_string(r) + "\n";
+//                    printf("%s", error_m.c_str());
+//                    throw logic_error(error_m.c_str());
+//                }
+//            }
+//
+//            in_file.close();
+//        }
+//    }
+//
+//    //printf("Loading tables took %f time.\n", omp_get_wtime() - start_time);
+//}
+
 /**
  * Read pre-computed ROF tables.
  * @param folder Folder containing the ROF tables.
@@ -190,92 +284,60 @@ Problem::Problem(unsigned long n_weeks) : n_weeks(n_weeks) {
 void
 Problem::setRofTables(unsigned long n_realizations, string rof_tables_directory) {
 
-    set<unsigned long> s( realizations_to_run.begin(), realizations_to_run.end() );
-    vector<unsigned long> realizations_to_run_load_tables;
-    realizations_to_run_load_tables.assign( s.begin(), s.end() );
+    double start_time = omp_get_wtime();
+    printf("Reading ROF tables.\n");
+    string file_name = rof_tables_directory + "tables_r0_u0.csv";
+    auto data_r0_u0 = Utils::parse2DCsvFile(file_name);
+    auto n_weeks_in_table = (int) data_r0_u0.size();
+    auto n_tiers = (int) data_r0_u0.at(0).size();
 
-    //double start_time = omp_get_wtime();
-    cout << "Loading ROF tables" << endl;
-    int n_tiers = NO_OF_INSURANCE_STORAGE_TIERS + 1;
-    auto table_data_id = realizations_to_run_load_tables[0];
-
-    /// Get number of weeks in tables
-    string file_name = rof_tables_directory + "/tables_r" + to_string(table_data_id) + "_u0";
-    ifstream in(file_name, ios_base::binary);
-    if (!in.good()) {
-        string error_table_file = "Tables file not found: " + file_name;
-        throw invalid_argument(error_table_file.c_str());
+    if (n_tiers != NO_OF_INSURANCE_STORAGE_TIERS) {
+        char error[75];
+        sprintf(error, "Number of tiers in tables does not match number of tiers for this problem.");
     }
 
-    //FIXME: FIGURE OUT NUMBER OF UTILITIES BY CHECKING IF TABLES_R0_U0, TABLES_R0_U1, AND SO ON EXIST.
-    char table_file_name[150];
-    for (n_utilities = 0; n_utilities < MAX_NUMBER_OF_UTILITIES; ++n_utilities) {
-        sprintf(table_file_name, "%s%stables_r%lu_u%d",
-                rof_tables_directory.c_str(), BAR.c_str(), table_data_id, n_utilities);
-        ifstream f(table_file_name);
-        if (!f.good()) {
-            if (n_utilities == 0) {
-                char error[200];
-                sprintf(error, "Table %s not found.", table_file_name);
-                throw invalid_argument(error);
-            }
-            break;
-        }
+    n_utilities = 0;
+    string fname = rof_tables_directory + "tables_r0_u0.csv";
+    while (access( fname.c_str(), F_OK ) != -1) {
+        n_utilities += 1;
+        fname = rof_tables_directory + "tables_r0_u" + to_string(n_utilities) + ".csv";
     }
 
-    unsigned n_weeks_in_table;
-    in.read(reinterpret_cast<char *>(&n_weeks_in_table), sizeof(unsigned));
-
-    // Create empty tables
     rof_tables = vector<vector<Matrix2D<double>>>(
             n_realizations,
             vector<Matrix2D<double>>((unsigned long) n_utilities,
-                                     Matrix2D<double>(n_weeks_in_table / n_tiers, n_tiers)));
+                                     Matrix2D<double>(n_weeks_in_table, n_tiers)));
 
-    this->rof_tables_directory = rof_tables_directory;
-
-    // Load ROF tables
-    for (auto r : realizations_to_run_load_tables) {
+    for (unsigned long r = 0; r < n_realizations; ++r) {
         for (int u = 0; u < n_utilities; ++u) {
-            file_name = rof_tables_directory + BAR + "tables_r" + to_string(r) + "_u" + to_string(u);
-            ifstream in_file(file_name, ios_base::binary);
-            if (!in.good()) {
-                string error_table_file = "Tables file not found: " + file_name;
-                throw invalid_argument(error_table_file.c_str());
+            string file_name = rof_tables_directory + "tables_r" + to_string(r) + "_u" + to_string(u) + ".csv";
+            auto tables_utility_week = Utils::parse2DCsvFile(file_name);
+
+            for (int w = 0; w < n_weeks; ++w) {
+                rof_tables[r][u].setPartialData(w, tables_utility_week[w].data(), tables_utility_week[w].size());
             }
-
-            // Get table file size from table files.
-            unsigned stringsize;
-            in_file.read(reinterpret_cast<char *>(&stringsize), sizeof(unsigned));
-
-            // Get table information from table files.
-            double data[stringsize];
-            in_file.read(reinterpret_cast<char *>(&data),
-                    stringsize * sizeof(double));
-
-            // Create tables based on table files.
-            rof_tables[r][u].setData(data, (int) stringsize);
-
-            for (unsigned long i = 0; i < stringsize; ++i) {
-                double d = data[i];
-                if (std::isnan(d) || d > 1.01 || d < 0) {
-                    string error_m = "nan or out of [0,1] rof imported "
-                                     "tables. Realization " +
-                                     to_string(r) + "\n";
-                    printf("%s", error_m.c_str());
-                    throw logic_error(error_m.c_str());
-                }
-            }
-
-            in_file.close();
+//        u = 1;
+//        file_name = rof_tables_directory + "tables_r" + to_string(r) + "_u" + to_string(u) + ".csv";
+//        tables_utility_week = Utils::parse2DCsvFile(file_name);
+//
+//        for (int w = 0; w < n_weeks; ++w) {
+//            rof_tables[r][u].setPartialData(w, tables_utility_week[w].data(), tables_utility_week[w].size());
+//        }
+//        u = 2;
+//        file_name = rof_tables_directory + "tables_r" + to_string(r) + "_u" + to_string(u) + ".csv";
+//        tables_utility_week = Utils::parse2DCsvFile(file_name);
+//
+//        for (int w = 0; w < n_weeks; ++w) {
+//            rof_tables[r][u].setPartialData(w, tables_utility_week[w].data(), tables_utility_week[w].size());
+//        }
         }
     }
 
-    //printf("Loading tables took %f time.\n", omp_get_wtime() - start_time);
+    printf("Loading tables took %f time.\n", omp_get_wtime() - start_time);
 }
 
 
-void Problem::setImport_export_rof_tables(int import_export_rof_tables, int n_weeks, string rof_tables_directory) {
+void Problem::setImport_export_rof_tables(int import_export_rof_tables, string rof_tables_directory) {
     if (std::abs(import_export_rof_tables) > 1)
         throw invalid_argument("Import/export ROF tables can be assigned as:\n"
                                "-1 - import tables\n"

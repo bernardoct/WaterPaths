a682f923371ec82566ba4b0fadd9491916550a2d
sk8forether <pzv2@cornell.edu>
2019-10-11 20:53:40 +0000

    Moving Remora and mpiP to remora.nix

--- ContainerFiles/dev-env.nix
+++ ContainerFiles/dev-env.nix
@@ -3,44 +3,6 @@ with import <nixpkgs> { };
 # with import ((builtins.getEnv "HOME") + "/workspace/nixpkgs") { }; # or:
 # with import "../nixpkgs" { };
 # Note taht the above are not accessible during docker build
-let
-  mpiP = stdenv.mkDerivation {
-    name = "mpiP";
-    
-    src = fetchurl {
-      url = https://sourceforge.net/projects/mpip/files/mpiP/mpiP-3.4.1/mpiP-3.4.1.tar.gz;
-      sha256 = "1w4n693im81qgbq0m8f0cv8nd8l2h8srriczby8nl7i1fdyz72v8";
-    };
-    phases = "installPhase";
-    
-    installPhase = ''
-      mkdir -p $out/
-      tar -C $out -xzf $src
-    '';
-  };
-  remora = stdenv.mkDerivation {
-    name = "remora";
-    
-    src = fetchurl {
-      url = https://github.com/TACC/remora/archive/v1.8.3.tar.gz;
-      sha256 = "08n72i6r8y14zr5wlcllpccv4q9xz6lf0j2d5k7kmyz55qid8kb8";
-    };
-    phases = "installPhase";
-    
-    buildInputs = with stdenv; [ binutils gcc gfortran openmpi ];
-    
-    installPhase = ''
-      source $stdenv/setup
-      
-      mkdir -p $out/
-      tar -C $out -xzf $src
-      
-      export REMORA_INSTALL_PREFIX=$out
-      cd $out/remora-1.8.3
-      $out/remora-1.8.3/install.sh
-    '';
-  };
-in
 { openmpiDevEnv = buildEnv {
   name = "openmpi-dev-env";
   paths = [
@@ -61,23 +23,9 @@ in
     gfortran
     openmpi
     openssh
-    
-    #
-    # Debug and profiling tools
-    #
-    mpiP
-    remora
-    
-    ];
-  };
-  shellHook = ''
-    export FOO="BAR"
-    export PATH="$PATH:${remora.out}/bin"
-    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${remora.out}/lib"
-    export REMORA_BIN="${remora.out}/bin"
-  '';
-}
+  ];
 
+};}
 
 #######################################
 #
